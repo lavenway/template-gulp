57a6a39e0b262615f60cd58bb2a5aea13449cf52
Ben Fowler <benfowler@bens-mbp.home>
2016-01-14 09:42:15 +0000

    Added body copy component

--- Gruntfile.js
+++ Gruntfile.js
@@ -82,7 +82,8 @@ module.exports = function (grunt) {
             'dist/template1.html': 'templates/layout/template1.html',
             'dist/header-image.html': 'templates/components/header-image.html',
             'dist/standfirst.html': 'templates/components/standfirst.html',
-            'dist/title.html': 'templates/components/title.html'
+            'dist/title.html': 'templates/components/title.html',
+            'dist/bodycopy.html': 'templates/components/bodycopy.html'
         }
       }
     },
