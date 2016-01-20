c483eb62197ce821623c2259cec43bac14a381e0
rhapsodygitdev <Ben@ben.local>
2016-01-20 10:34:18 +0000

    Added list three component

--- Gruntfile.js
+++ Gruntfile.js
@@ -94,7 +94,8 @@ module.exports = function (grunt) {
             'dist/bookit.html': 'templates/components/bookit.html',
             'dist/related-articles.html': 'templates/components/related-articles.html',
             'dist/image.html': 'templates/components/image.html',
-            'dist/list-one.html': 'templates/components/list-one.html'
+            'dist/list-one.html': 'templates/components/list-one.html',
+            'dist/list-three.html': 'templates/components/list-three.html'
         }
       }
     },
