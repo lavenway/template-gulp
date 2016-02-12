1c2c37dd6768433a1ec588fae43cc7b6f41eaa2b
William Rodil <william@masondigital.co.uk>
2016-02-12 13:14:28 +0000

    Wills update to Trent tool

--- assets/js/framework/trend-tool.js
+++ assets/js/framework/trend-tool.js
@@ -54,26 +54,32 @@ $(function () {
 	}
 	
 	statusBar()
-	
- 
-
+	// Option animate on load
+	function animateOption() {
+		setTimeout(function() {
+	        $('.options .option').addClass('appear');
+	    }, 1700);
+	}
 	// Toggle between sections - follow the rabit
 	$('#tt-section0').click(function() {
 		$(this).removeClass('tt-active');
 		$('#tt-section1').addClass('tt-active tt-opened');
-		showStatusBar()
+		showStatusBar();
+		animateOption();
 	});
 	$('.option-age').click(function() {
 		var mainSection = $('.tt-section');
 		mainSection.removeClass('tt-active');
 		$('#tt-section2').addClass('tt-active tt-opened');
-		showStatusBar()
+		showStatusBar();
+		animateOption();
 	});
 	$('.option-tone').click(function() {
 		var mainSection = $('.tt-section');
 		mainSection.removeClass('tt-active');
 		$('#tt-section3').addClass('tt-active tt-opened');
-		showStatusBar()
+		showStatusBar();
+		animateOption();
 	});
 	$('.option-eye').click(function() {
 		var mainSection = $('.tt-section');
@@ -84,7 +90,7 @@ $(function () {
 
 	// Custom Scroll
 	function customScroll() {
-		var ttoolHeight = $('.ttool').height()-100;
+		var ttoolHeight = $('.ttool').height()-110;
 		$(".options").mCustomScrollbar({
 			setHeight: ttoolHeight,
 			theme:"inset-2-dark",
@@ -100,10 +106,10 @@ $(function () {
 	customScroll();
 
 	// Divide and apply width to the Result tabs navigation
-	var resultTabs = $('.result-tabs'); 
-	var tabPane = $('.tabs-below .tab-pane');
-	var tabPaneCount = tabPane.length;
-	resultTabs.children().css('width',100/tabPaneCount+'%');
+	// var resultTabs = $('.result-tabs'); 
+	// var tabPane = $('.tabs-below .tab-pane');
+	// var tabPaneCount = tabPane.length;
+	// resultTabs.children().css('width',100/tabPaneCount+'%');
 
 	var resultFilterNav = $('.result-filter-nav li');
 	var resultFilterBody = $('.result-filter-body');
@@ -115,12 +121,10 @@ $(function () {
 		$('#'+rfId).addClass('rf-active');
 		$('#copy-'+rfId).addClass('rf-active');
 	});
-
 	$('.result-tabs a').click(function (e) {
-	  e.preventDefault()
-	  $(this).tab('show')
+		e.preventDefault()
+	 	$(this).tab('show')
 	})
-
 	$('.result-tabs a.product-kit').click(function (e) {
 		ttProductCarousel(); 
 	 	$('.tt-products-carousel').trigger('resize');
