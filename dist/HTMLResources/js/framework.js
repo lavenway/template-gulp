c9493bea2a1b46a171c9b8e7c806b850a7985b5e
William Rodil <william@masondigital.co.uk>
2016-02-15 01:30:37 +0000

    Will amend for snags and changes for Basket

--- dist/HTMLResources/js/framework.js
+++ dist/HTMLResources/js/framework.js
@@ -567,6 +567,17 @@ jQuery('body').on('click', '.terms_modal .close,.success_modal .close', function
 
 /*---------- END COMPETITION COMPONENT ----------*/
 
+// Pop up on Add click
+
+jQuery('.pg-add-btn').click(function() {
+  jQuery('.add-basket-notice').addClass('abn-display');
+  setTimeout(function() {
+      jQuery('.add-basket-notice').removeClass('abn-display');;
+  }, 2000);
+});
+jQuery('.abn-close-btn').click(function() {
+  jQuery('.add-basket-notice').removeClass('abn-display');
+});
 
 'use strict';
 $ = $ || jQuery;
